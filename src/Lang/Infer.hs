72a15efbe2360d66505101e80ec1d528d3d6fb2a
petroniuss <wojtyczekPatryk@gmail.com>
2020-09-03 17:25:04 +0200

    impl hindley milner -- nasty

--- src/Lang/Infer.hs
+++ src/Lang/Infer.hs
@@ -17,6 +17,8 @@ import qualified Data.Set               as Set
 
 data TypeError
     = UnboundVariable String
+    | UnificationFail Type Type
+    | InifiniteType TVar Type
 
 -- Fresh names for our types
 
@@ -32,6 +34,11 @@ freshName = do
     put others
     return name
 
+fresh :: Infer Type
+fresh = do
+    name <- lift $ freshName
+    return $ TVar $ TV name
+
 inifiniteNamesSupply :: [String]
 inifiniteNamesSupply = [1..] >>= flip replicateM ['a'..'z']
 
@@ -39,18 +46,17 @@ inifiniteNamesSupply = [1..] >>= flip replicateM ['a'..'z']
 
 type Infer a = ExceptT TypeError (NamesSupply) a
 type TypeEnv = Map.Map Name Scheme
+
+
+---------------- Substitution --------------------
 type Subst = Map.Map TVar Type
-
-
--- We're going to perform subsitution over the structure replacing type variables as specified
--- in subsitution
 
 emptySubst :: Subst
 emptySubst = Map.empty
 
 -- I'm gonna need a second to comprehend this line
 compose :: Subst -> Subst -> Subst
-s1 `compose` s2 = Map.map (apply s1) s2 `Map.union` s1
+s1 `compose` s2 = (Map.map (apply s1) s2) `Map.union` s1
 
 class Substitutable a where
     -- Applies given substitution over the structure
@@ -78,6 +84,7 @@ instance Substitutable Type where
 
 instance Substitutable Scheme where
     -- I don't know why this is done this way>?>
+    -- I apply modified substitution with all elements from tvars deleted, over the type
     apply sbst (Forall tvars t) = Forall tvars $ apply s' t
         where s' = foldr Map.delete sbst tvars
 
@@ -94,6 +101,112 @@ instance Substitutable TypeEnv where
     apply = Map.map . apply
 
     ftv = ftv . Map.elems
+
+
+--------------------- Unification -----------------------------
+
+occursCheck :: Substitutable a => TVar -> a -> Bool
+occursCheck a t = a `Set.member` ftv t
+
+unify :: Type -> Type -> Infer Subst
+unify (l `TArr` r) (l' `TArr` r') = do
+    s1 <- unify l l'
+    s2 <- unify (apply s1 r) (apply s1 r')
+    return $ s2 `compose` s1
+
+unify (TVar a) t = bind a t
+unify t (TVar a) = bind a t
+unify (TCon a) (TCon b) | a == b = return emptySubst
+unify t1 t2 = throwError $ UnificationFail t1 t2
+
+bind :: TVar -> Type -> Infer Subst
+bind a t | t == TVar a = return emptySubst
+         | occursCheck a t = throwError $ InifiniteType a t
+         | otherwise = return $ Map.singleton a t
+
+---------------------------------------------------------------
+
+---------------- Generealization and Instantiation ------------
+
+instantiate :: Scheme -> Infer Type
+instantiate (Forall as t) = do
+    as' <- mapM (const fresh) as
+    let s = Map.fromList $ zip as as'
+    return $ apply s t
+
+generalize :: TypeEnv -> Type -> Scheme
+generalize env t = Forall as t
+    where as = Set.toList $ ftv t `Set.difference` ftv env
+
+
+----------------- Typing Rules --------------------------------
+
+lookupEnv :: TypeEnv -> Name -> Infer (Subst, Type)
+lookupEnv env name = do
+    case Map.lookup name env of
+        Nothing -> throwError $ UnboundVariable (show name)
+        Just s  -> do t <- instantiate s
+                      return (emptySubst, t)
+
+extend :: TypeEnv -> (Name, Scheme) -> TypeEnv
+extend env (x, s) = Map.insert x s env
+
+emptyTypeEnv :: TypeEnv
+emptyTypeEnv = Map.empty
+
+typeof :: TypeEnv -> Name -> Maybe Scheme
+typeof env name = Map.lookup name env
+
+ops :: Map.Map BinOp Type
+ops = Map.fromList [
+    (Add, nArrow 3 typeInt),
+    (Sub, nArrow 3 typeInt),
+    (Mul, nArrow 3 typeInt),
+    (Eql, (typeInt `TArr` (typeInt `TArr` typeBool)))]
+        where
+            nArrow :: Int -> Type -> Type
+            nArrow 1 tpe = tpe
+            nArrow n tpe = tpe `TArr` (nArrow (n - 1) tpe)
+
+typeArrow :: Type -> Type -> Type -> Type
+typeArrow t1 t2 t3 = t1 `TArr` (t2 `TArr` t3)
+
+
+infer :: TypeEnv -> Expr -> Infer (Subst, Type)
+infer env ex = case ex of
+    Var x -> lookupEnv env x
+
+    Lam x e -> do
+        tv <- fresh
+        let env' = env `extend` (x, Forall [] tv)
+        (s1, t1) <- infer env' e
+        return $ (s1, apply s1 tv `TArr` t1)
+
+    App e1 e2 -> do
+        tv <- fresh
+        (s1, t1) <- infer env e1
+        (s2, t2) <- infer (apply s1 env) e2
+
+        s3       <- unify (apply s2 t1) (t2 `TArr` tv)
+        return (s3 `compose` s2 `compose` s1, apply s3 tv)
+
+    Let x e1 e2 -> do
+        (s1, t1) <- infer env e1
+        let env' = apply s1 env
+            t'   = generalize env' t1
+        (s2, t2) <- infer (env' `extend` (x, t')) e2
+        return (s1 `compose` s2, t2)
+
+    Op op e1 e2 -> do
+        (s1, t1) <- infer env e1
+        (s2, t2) <- infer env e2
+        tv <- fresh
+        s3 <- unify (typeArrow t1 t2 tv) (ops Map.! op)
+        return (s1 `compose` s2 `compose` s3, apply s3 tv)
+
+
+
+
 
 
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,13 @@
+import           Lang.Infer
+import           Test.Hspec
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+
+
+-- simple :: Spec
+
+-- simple :: Spec
+-- simple = describe "Tests for substitution.." $ do
+--     it "foo" $
+
