ad54534dd42f1217c9985c5f2c50766e4d440a63
petroniuss <wojtyczekPatryk@gmail.com>
2020-09-07 16:20:19 +0200

    implemented type infering (only dumb names without solving

--- src/Lang/Infer.hs
+++ src/Lang/Infer.hs
@@ -10,6 +10,7 @@ module Lang.Infer where
     -- )
 
 import           Lang.Syntax
+import           Lang.TypeEnv           as TypeEnv
 
 import           Control.Applicative
 import           Control.Monad.Except
@@ -18,6 +19,7 @@ import           Control.Monad.State
 import           Data.Monoid
 
 import           Control.Monad.Identity (Identity)
+import           Control.Monad.RWS      (RWST, ask, local, tell)
 import qualified Data.Map               as Map
 import qualified Data.Set               as Set
 
@@ -36,10 +38,14 @@ data TypeError
     -- We throw this one when subsitution would result in an infinite type.
     -- Example: unifying typevar a and type a -> b
     | InifiniteType TypeVar Type
+    -- There's no unique solution to given set of constraints
+    | Ambigious [Constraint]
+    -- Duunot know :)
+    | UnificationMismatch [Type] [Type]
+
 
 
 -- type Infer a = ExceptT TypeError (NamesSupply) a
-type TypeEnv = Map.Map String TypeScheme
 
 ---------------- Substitution --------------------
 -- type Subst = Map.Map TVar Type
@@ -53,6 +59,127 @@ type TypeEnv = Map.Map String TypeScheme
 
 -------------------------------------------------
 
+type Constraint = (Type, Type)
 
--- Generate set of constraints
--- infer :: Expr -> Infer Type
+-- We generate constraints using stateful generation of new names
+type Infer a = RWST TypeEnv [Constraint] [String] (Except TypeError) a
+
+-- Generating type names
+instance MonadFail Identity where
+    fail = fail
+
+freshType :: Infer Type
+freshType = do
+    var <- freshTypeVar
+    return $ TVar var
+
+
+freshTypeVar :: Infer TypeVar
+freshTypeVar = do
+    name <- freshName
+    return $ TypeVar name
+
+freshName :: Infer String
+freshName = do
+    (name:others) <- get
+    put others
+    return name
+
+inifiniteNamesSupply :: [String]
+inifiniteNamesSupply = [1..] >>= flip replicateM ['a'..'z']
+
+----
+
+addConstraint :: (Type , Type) -> Infer ()
+addConstraint (t1,t2) = do tell [(t1, t2)]
+
+
+locally :: (String, TypeScheme) -> Infer a -> Infer a
+locally (name, scheme) inf = do
+    let trans e = Map.insert name scheme e
+    local trans inf
+
+
+emptyScheme :: Type -> TypeScheme
+emptyScheme var = Forall [] var
+
+
+generalize :: TypeEnv -> Type -> TypeScheme
+generalize tpEnv tpe = Forall vars tpe
+    where vars = Set.toList $ ftv tpe `Set.difference` ftv tpEnv
+
+----
+
+type Subsitution = Map.Map TypeVar Type
+
+class Substitutable a where
+    -- Returns set of all free type variables
+    ftv :: a -> Set.Set TypeVar
+
+    -- Replaces all occurences of free type variables with given type
+
+-- TODO implement all these subsitutable
+
+
+infer :: Expr -> Infer Type
+infer expr =
+    case expr of
+        Var name -> do
+            env <- ask
+            case TypeEnv.lookup env name of
+                Nothing             -> throwError $ UnboundVariable name
+                Just (Forall _ tpe) -> return tpe
+
+        App e1 e2 -> do
+            t1 <- infer e1
+            t2 <- infer e2
+            resultT <- freshType
+            addConstraint (t1, t2 `TArr` resultT)
+            return resultT
+
+        Lam name expr -> do
+            argT <- freshType
+            res <- locally (name, emptyScheme argT) $ infer expr
+            return (argT `TArr` res)
+
+        Let name e1 e2 -> do
+            t1 <- infer e1
+            env <- ask
+            let generalized = generalize env t1
+            locally (name, generalized) $ infer e2
+
+        Lit (LInt _)  -> return typeInt
+
+        Lit (LBool _) -> return typeBool
+
+        If condE trE flE -> do
+            t1 <- infer condE
+            t2 <- infer trE
+            t3 <- infer flE
+            addConstraint (t1, typeBool)
+            addConstraint (t2, t3)
+            return t2
+
+        Fix e1 -> do
+            t1 <- infer e1
+            tv <- freshType
+            addConstraint (tv `TArr` tv, t1)
+            return tv
+
+        Op binop e1 e2 -> do
+            t1 <- infer e1
+            t2 <- infer e2
+            resT <- freshType
+            let actual = t1 `TArr` t2 `TArr` resT
+                expected = opType binop
+
+            addConstraint (actual, expected)
+            return resT
+
+
+opType :: BinOp -> Type
+opType Add = typeInt `TArr` typeInt `TArr` typeInt
+opType Sub = typeInt `TArr` typeInt `TArr` typeInt
+opType Mul = typeInt `TArr` typeInt `TArr` typeInt
+opType Eql = typeInt `TArr` typeInt `TArr` typeBool
+
