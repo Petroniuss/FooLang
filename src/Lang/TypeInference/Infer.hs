19e3b1358f272bc7c5426907deb56be9397144c5
petroniuss <wojtyczekPatryk@gmail.com>
2020-09-10 15:08:33 +0200

    refactored infer module

--- src/Lang/TypeInference/Infer.hs
+++ src/Lang/TypeInference/Infer.hs
@@ -21,7 +21,7 @@ import           Lang.Utils.Util
 ------------------------------------------------------------------------
 
 {-
-    This module is single interface for Type Inference.
+    This module is a single interface for Type Inference.
 -}
 
 ------------------------------------------------------------------------
